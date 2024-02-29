c58b497bb15cdbf741d23b69142e3126699a3c3d
VitaAtiv <142582763+VitaAtiv@users.noreply.github.com>
2024-02-29 16:09:37 +0200

    add

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 const refs = {
     form: document.querySelector(".loginForm"),
 }
-console.log(form);
-refs.form.addEventListener("submit", )
+console.log(refs);
+refs.form.addEventListener("submit", onSubmit)
+
+function onSubmit(event) {
+    event.preventDefault();
+
+
+    console.log(event.currentTarget.login.value);
+    console.log(event.currentTarget.gender);
+    
+    // console.log(event.currentTarget.gender.value);
+    // const login = event.currentTarget.login.value;
+    // const gender = event.currentTarget.gender.value;
+    // const data = {
+    //   login,
+    //   gender,
+    // };
+
+    const data = new FormData(event.currentTarget);
+    console.log(data);
+    const validData = {};
+    data.forEach((value, key) => {
+        console.log(key, value);
+        validData["val"] = 1;
+    });
+}
