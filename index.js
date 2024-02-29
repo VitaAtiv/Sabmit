96efed26c160cf50019b94ffc062ad9ca44ecf23
VitaAtiv <142582763+VitaAtiv@users.noreply.github.com>
2024-02-29 17:27:27 +0200

    add

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*
+
 const refs = {
     form: document.querySelector(".loginForm"),
 }
@@ -27,3 +29,61 @@ function onSubmit(event) {
         validData["val"] = 1;
     });
 }
+
+*/
+
+const refs = {
+  form: document.querySelector(".box"),
+};
+
+
+function onSubmit(event) {
+    event.preventDefault();
+
+    /*
+    const city = form.querySelector ('[name="city"]')
+    const name = form.querySelector('[name="name"]');
+
+    const values = {
+      city: city.value,
+      name: name.value,
+    };
+
+    console.log("variant1", values);
+*/
+    
+    
+   /*
+    const fields = document.querySelectorAll('input, select, textarea');
+    const values = {};
+
+    fields.forEach (field => 
+    const {name, value} = field;
+        values[name] = value;
+         )
+
+    console.log("variant2", values);
+*/
+
+
+    const { city, name } = form;
+    const values = {
+      city: city.value,
+      name: name.value,
+    };
+console.log("variant3", values);
+
+    // const data = new FormData(event.currentTarget);
+    // console.log(data);
+    // const validData = {};
+    // data.forEach((value, key) => {
+    //   console.log(key, value);
+    //     validData["val"] = 1;
+    //     validData.val = 1;
+    // });
+}
+
+refs.form.addEventListener("submit", onSubmit);
+
+
+
