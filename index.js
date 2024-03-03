9d7a969fb9e40f0a438ad79c6d5ab88a015668a4
VitaAtiv <142582763+VitaAtiv@users.noreply.github.com>
2024-03-03 13:54:06 +0200

    add

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const refs = {
   form: document.querySelector(".box"),
-
 };
 
 function onSubmit(event) {
@@ -13,15 +12,20 @@ function onSubmit(event) {
     console.log(key, value);
     validData[key] = value;
   });
-    console.log(validData);
+  console.log(validData);
 }
 
 refs.form.addEventListener("submit", onSubmit);
 
 
 
+
+
+
 const inputEl = document.querySelector(".inputEmail");
+const textEl = document.querySelector(".textEmail");
 console.log(inputEl);
+console.log(textEl);
 
 // подія change
 inputEl.addEventListener("change", onChange);
@@ -33,5 +37,28 @@ function onChange(event) {
 // подія input
 inputEl.addEventListener("input", onInput);
 function onInput(event) {
-  console.log(event);
+  // через length
+
+  // const text = event.target.value;
+  // console.log(text.length);
+  // if (text.length === 0) {
+  //   textEl.textContent = "Nothing";
+  //   return
+  // }
+  // textEl.textContent = text;
+
+  // через оператор або
+
+  const text = event.target.value || "Nothing";
+console.log(inputEl.classList.contains(".not-valid"));
+  if (text.length < 5) {
+    inputEl.classList.add("not-valid");
+  } else {
+    inputEl.classList.remove("not-valid");
+  }
+  textEl.textContent = text;
+  // через тернарну функцію
+
+  // const text = event.target.value.length === 0 ? "Nothing" : event.target.value;
+  // textEl.textContent = text;
 }
